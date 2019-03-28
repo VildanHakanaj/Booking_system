ab34132f89e5e90d37b8a765321e1f7a2ceda74a
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2019-03-28 15:03:57 -0400

    Added the booking table and the index page for the booking

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Booking;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    /*
+     *TODO::
+     * [ ] Show all the booking that exist in the booking page
+     * [ ] Allow the user to create the booking and store it
+     * [ ] Allow the booking to get canceled
+     * [ ] Have a booking settings pages to show
+     * [ ] Allow the admin to choose what the days the checkin and out are.
+     *      [ ] Show the calendar of the dates and allow the admin to check the dates they don't want
+     *      [ ] After the admin has selected the dates that they don't want then show the actual calendar that the user
+     *          be able to see.
+     * CALENDAR
+     * [ ] The calendar will be able to be clicked on and place event listeners on what to do on that dates
+     * [ ] The
+     *
+     * */
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $bookings = Booking::orderBy('created_at', 'DESC')->paginate(10);
+        return view('admin.bookings.index')->with('bookings', $bookings);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Booking $booking)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_03_27_221652_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('kit_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('checked_out')->nullable();
+            $table->boolean('checked_in')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
